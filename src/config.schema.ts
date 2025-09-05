11b6c0f7192c55919a04973deb7726e3379e4abc
dokibal <doktor.balazs1@gmail.com>
2025-09-05 07:57:05 +0200

    Get frontend url from env variable

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -3,4 +3,5 @@ import * as Joi from 'joi';
 export const configValidationSchema = Joi.object({
   DEEPSEEK_API_KEY: Joi.string().required(),
   DEEPSEEK_BASE_URL: Joi.string().required(),
+  FRONTEND_URL: Joi.string().uri().required(),
 });
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: configService.get<string>('FRONTEND_URL'),
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => {
